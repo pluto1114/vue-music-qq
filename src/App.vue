1e99b2c636f7d0782dcac31ffc84bd29b67086e2
pluto1114 <xiongmao1114@163.com>
2017-02-03 23:52:02 +0800

    test muse-ui

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <mu-raised-button @click="openBottomSheet" label="Open Bottom Sheet" />
+    <mu-bottom-sheet :open="bottomSheet" @close="closeBottomSheet">
+      <mu-list @itemClick="closeBottomSheet">
+        <mu-sub-header>
+          请选择一个
+        </mu-sub-header>
+        <mu-list-item title="阴阳师"/>
+        <mu-list-item title="贪吃蛇大作战"/>
+        <mu-list-item title="一划到底"/>
+        <mu-list-item title="全民斗地主"/>
+      </mu-list>
+    </mu-bottom-sheet>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
-  name: 'app',
-  components: {
-    Hello
+  data () {
+    return {
+      bottomSheet: false
+    }
+  },
+  methods: {
+    closeBottomSheet () {
+      this.bottomSheet = false
+    },
+    openBottomSheet () {
+      this.bottomSheet = true
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+Vue.use(MuseUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
